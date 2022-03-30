b8a746a8da1575426df58e5c8bf2b9e4eacda15c
盛光晟 <singlesaulwork@gmail.com>
2022-03-30 17:57:57 +0800

    make layout

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -7,19 +7,26 @@
 			</view>
 		</u-sticky>
 		<!-- #ifndef APP-NVUE || MP-TOUTIAO -->
-		    <view class="u-swiper-block">
-		        <u-swiper
-		                :list="list3"
-		                previousMargin="30"
-		                nextMargin="30"
-		                circular
-		                :autoplay="true"
-		                radius="5"
-		                bgColor="#ffffff"
-		        ></u-swiper>
-		    </view>
-		    <!-- #endif -->
-		<u-tabbar :value="value6" @change="name => value6 = name" :fixed="true" :placeholder="true"
+		<view class="u-swiper-block">
+			<u-swiper :list="list3" previousMargin="30" nextMargin="30" circular :autoplay="true" radius="5"
+				bgColor="#ffffff"></u-swiper>
+		</view>
+		<!-- #endif -->
+		<view class="scroll-wrapper">
+			<u-scroll-list indicatorActiveColor="#ffbb00">
+				<view class="scroll-list">
+					<view class="scroll-list__line" v-for="(item, index) in menuArr" :key="index">
+						<view class="scroll-list__line__item" v-for="(item1, index1) in item" :key="index1"
+							:class="[(index1 === item.length - 1) && 'scroll-list__line__item--no-margin-right']">
+							<image class="scroll-list__line__item__image" :src="menuBaseUrl + item1.icon" mode=""></image>
+							<text class="scroll-list__line__item__text">{{ item1.name }}</text>
+						</view>
+					</view>
+				</view>
+			</u-scroll-list>
+		</view>
+	
+		<u-tabbar :value="selected" :fixed="true" :placeholder="true" @change="handleChangeIndex"
 			:safeAreaInsetBottom="true" activeColor="#ffbb00" inactiveColor="#333">
 			<u-tabbar-item text="首页" icon="home-fill"></u-tabbar-item>
 			<u-tabbar-item text="记账" icon="red-packet-fill"></u-tabbar-item>
@@ -35,43 +42,136 @@
 	export default Vue.extend({
 		data() {
 			return {
+				menuBaseUrl: 'https://cdn.uviewui.com/uview/menu/',
 				title: 'Hello',
-				value6: 0,
+				selected: 0,
 				list3: [
 					'https://cdn.uviewui.com/uview/swiper/swiper3.png',
 					'https://cdn.uviewui.com/uview/swiper/swiper2.png',
 					'https://cdn.uviewui.com/uview/swiper/swiper1.png',
+				],
+				urls: [
+					'/pages/index/index',
+					'/pages/money/Money'
+				],
+				menuArr: [
+					[{
+							name: '天猫新品',
+							icon: '11.png'
+						},
+						{
+							name: '今日爆款',
+							icon: '9.png'
+						}, {
+							name: '天猫国际',
+							icon: '17.png'
+						}, {
+							name: '饿了么',
+							icon: '6.png'
+						}, {
+							name: '天猫超市',
+							icon: '11.png'
+						}, {
+							name: '分类',
+							icon: '2.png'
+						}, {
+							name: '天猫美食',
+							icon: '3.png'
+						}, {
+							name: '阿里健康',
+							icon: '12.png'
+						}, {
+							name: '口碑生活',
+							icon: '7.png'
+						}
+					],
+					[{
+							name: '充值中心',
+							icon: '8.png'
+						},
+						{
+							name: '机票酒店',
+							icon: '10.png'
+						}, {
+							name: '金币庄园',
+							icon: '18.png'
+						}, {
+							name: '阿里拍卖',
+							icon: '15.png'
+						}, {
+							name: '淘宝吃货',
+							icon: '16.png'
+						}, {
+							name: '闲鱼',
+							icon: '4.png'
+						}, {
+							name: '会员中心',
+							icon: '6.png'
+						}, {
+							name: '造点新货',
+							icon: '13.png'
+						}, {
+							name: '土货鲜食',
+							icon: '14.png'
+						}
+					]
 				]
 			};
 		},
 		onLoad() {
 
 		},
-		methods: {}
+		onHide() {
+			this.selected = 0
+		},
+		methods: {
+			handleChangeIndex(index: number) {
+				console.log('12', index)
+				this.selected = index
+				uni.navigateTo({
+					url: this.urls[index],
+					fail: (e) => {
+						alert(e.errMsg)
+					}
+				})
+				console.log('1', index)
+			},
+			left() {
+				console.log('left');
+			},
+			right() {
+				console.log('right');
+			}
+		}
 	});
 </script>
 
 <style lang="scss" scoped>
 	@import '@/static/fonts/custom_fonts.css';
-	.home-content{
-		
-	}
+
+	.home-content {}
+
 	/* 轮播图 */
-	.u-swiper{
+	.u-swiper {
 		width: 100vw;
 	}
+
 	/* 头部 */
-	.home-head{
+	.home-head {
 		width: 100vw;
 		height: 200rpx;
 		position: relative;
 		/*! Edit and share this gradient: https://html5kit.com/gradient-editor/?preset=10,100;100,0;ff0088,0;ffbb00,0 */
-		background: -moz-linear-gradient(bottom, rgba(255,0,136,1) 0%, rgba(255,187,0,1) 0%, rgba(255,187,0,0.1) 100%); /* FF3.6+ */
-		background: -webkit-linear-gradient(bottom, rgba(255,0,136,1) 0%, rgba(255,187,0,1) 0%, rgba(255,187,0,0.1) 100%); /* Chrome10-25, Safari5.1-6 */
-		background: linear-gradient(to bottom, rgba(255,0,136,1) 0%, rgba(255,187,0,1) 0%, rgba(255,187,0,0.1) 100%); /* W3C, IE10+, Firefox16+, Chrome26+, Opera12+, Safari7+ */
-		filter: progid:DXImageTransform.Microsoft.gradient(rgba(255,0,136,1) 0%,rgba(255,187,0,1) 0%,rgba(255,187,0,0.1) 100%, GradientType=0); /* IE6-9 */
-		
-		.home-title{
+		background: -moz-linear-gradient(bottom, rgba(255, 0, 136, 1) 0%, rgba(255, 187, 0, 1) 0%, rgba(255, 187, 0, 0.1) 100%);
+		/* FF3.6+ */
+		background: -webkit-linear-gradient(bottom, rgba(255, 0, 136, 1) 0%, rgba(255, 187, 0, 1) 0%, rgba(255, 187, 0, 0.1) 100%);
+		/* Chrome10-25, Safari5.1-6 */
+		background: linear-gradient(to bottom, rgba(255, 0, 136, 1) 0%, rgba(255, 187, 0, 1) 0%, rgba(255, 187, 0, 0.1) 100%);
+		/* W3C, IE10+, Firefox16+, Chrome26+, Opera12+, Safari7+ */
+		filter: progid:DXImageTransform.Microsoft.gradient(rgba(255, 0, 136, 1) 0%, rgba(255, 187, 0, 1) 0%, rgba(255, 187, 0, 0.1) 100%, GradientType=0);
+		/* IE6-9 */
+
+		.home-title {
 			color: #333;
 			height: 200rpx;
 			line-height: 200rpx;
@@ -81,8 +181,8 @@
 			display: inline-block;
 			vertical-align: middle;
 		}
-		
-		.home-img{
+
+		.home-img {
 			position: absolute;
 			left: 18%;
 			top: 60rpx;
@@ -90,8 +190,47 @@
 			height: 80rpx;
 		}
 	}
+
+	.u-swiper-block {
+		margin-top: 120rpx;
+	}
 	
-	.u-swiper-block{
-		margin-top: 40rpx;
+	.scroll-wrapper{
+		width: 100%;
+	}
+
+	.scroll-list {
+		@include flex(column);
+		
+		&__line {
+		@include flex;
+		margin-top: 10px;
+		margin-left: 20rpx;
+
+		&__item {
+			margin-right: 20rpx;
+			width: 160rpx;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+
+			&__image {
+				width: 121rpx;
+				height: 96rpx;
+			}
+
+			&__text {
+				margin-top: 5px;
+				color: $u-content-color;
+				font-size: 12px;
+				text-align: center;
+			}
+
+			&--no-margin-right {
+				margin-right: 0;
+			}
+		}
+	}
 	}
 </style>
---
+++ src/pages/memo/Memo.vue
@@ -0,0 +1,47 @@
+<template>
+	<view>
+		<u-tabbar :value="selected" :fixed="true" :placeholder="true" @change="handleChangeIndex"
+			:safeAreaInsetBottom="true" activeColor="#ffbb00" inactiveColor="#333">
+			<u-tabbar-item text="首页" icon="home-fill"></u-tabbar-item>
+			<u-tabbar-item text="记账" icon="red-packet-fill"></u-tabbar-item>	
+			<u-tabbar-item text="备忘" icon="calendar-fill"></u-tabbar-item>
+			<u-tabbar-item text="我的" icon="account-fill"></u-tabbar-item>
+		</u-tabbar>
+	</view>
+</template>
+
+<script lang="ts">
+	import Vue from 'vue';
+	export default Vue.extend({
+		data() {
+			return {
+				selected: 2,
+				urls: [
+					'/pages/index/Index',
+					'/pages/money/Money',
+					'/pages/memo/Memo'
+				]
+			};
+		},
+		onHide(){
+			this.selected = 2
+		},
+		methods:{
+			handleChangeIndex(index:number){
+				console.log('12',index)
+				this.selected = index
+				uni.navigateTo({
+					url: this.urls[index],
+					fail: (e)=>{
+						alert(e.errMsg)
+					}
+				})
+				console.log('1',index)
+			}
+		}
+	})
+</script>
+
+<style lang="scss">
+
+</style>
