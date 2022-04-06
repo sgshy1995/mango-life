f84c0683a551a91950c9a23cca225e8cb6bf777e
盛光晟 <singlesaulwork@gmail.com>
2022-04-06 14:45:51 +0800

    calculator

---
+++ src/pages/money/Calculator.vue
@@ -0,0 +1,287 @@
+<template>
+	<u-popup :show="showCalculator" @close="close" class="calculator">
+		<u-row justify="flex-start" class="calculator-info">
+			<u-col class="note" textAlign="center" align="center" justify="center" span="6">
+				<image src="../../static/images/home/备注.png" style="width: 18px;height: 18px;margin-right: 10rpx;"></image>
+				<u--input
+				    placeholder="请输入备注..."
+				    border="none"
+				    v-model="inputNote"
+				></u--input>
+			</u-col>
+			<u-col class="money-num" textAlign="center" align="flex-end" justify="center" span="6">
+				{{ inputNum }}
+			</u-col>
+		</u-row>
+		<u-row justify="flex-start">
+			<u-col @click="handleInputNumber('7')" textAlign="center" align="center" justify="center" span="3">
+				7
+			</u-col>
+			<u-col @click="handleInputNumber('8')" textAlign="center" align="center" justify="center" span="3">
+				8
+			</u-col>
+			<u-col @click="handleInputNumber('9')" textAlign="center" align="center" justify="center" span="3">
+				9
+			</u-col>
+			<u-col textAlign="center" align="center" justify="center" span="3">
+				<view class="choose">
+					<image src="../../static/images/home/日历.png" style="width: 18px;height: 18px;"></image>
+					<text>今日</text>
+				</view>
+				<view class="choose" style="border-top: 1rpx solid #999999;margin-top: 2px;padding-top: 2px;">
+					<image :src="pickIcon" style="width: 18px;height: 18px;"></image>
+					<text>礼物</text>
+				</view>
+			</u-col>
+		</u-row>
+		<u-row justify="flex-start">
+			<u-col @click="handleInputNumber('4')" textAlign="center" align="center" justify="center" span="3">
+				4
+			</u-col>
+			<u-col @click="handleInputNumber('5')" textAlign="center" align="center" justify="center" span="3">
+				5
+			</u-col>
+			<u-col @click="handleInputNumber('6')" textAlign="center" align="center" justify="center" span="3">
+				6
+			</u-col>
+			<u-col @click="handleAdd" textAlign="center" align="center" justify="center" span="3">
+				+
+			</u-col>
+		</u-row>
+		<u-row justify="flex-start">
+			<u-col @click="handleInputNumber('1')" textAlign="center" align="center" justify="center" span="3">
+				1
+			</u-col>
+			<u-col @click="handleInputNumber('2')" textAlign="center" align="center" justify="center" span="3">
+				2
+			</u-col>
+			<u-col @click="handleInputNumber('3')" textAlign="center" align="center" justify="center" span="3">
+				3
+			</u-col>
+			<u-col @click="handleReduce" textAlign="center" align="center" justify="center" span="3">
+				-
+			</u-col>
+		</u-row>
+	    <u-row justify="flex-start">
+			<u-col @click="handleInputPoint" textAlign="center" align="center" justify="center" span="3">
+				.
+			</u-col>
+			<u-col @click="handleInputNumber('0')" textAlign="center" align="center" justify="center" span="3">
+				0
+			</u-col>
+			<u-col textAlign="center" align="center" justify="center" span="3" @click="handleDelete">
+				<image src="../../static/images/home/键盘回退.png" style="width: 18px;height: 18px;"></image>
+			</u-col>
+			<u-col v-if="cache" @click="handleEqual" textAlign="center" align="center" justify="center" span="3">
+				=
+			</u-col>
+			<u-col @click="handleOk" class="handle-submit" textAlign="center" align="center" justify="center" span="3">
+				确认
+			</u-col>
+		</u-row>
+	</u-popup>
+</template>
+
+<script lang="ts">
+	import Vue from 'vue';
+	import {toDecimal2} from '../../utils/utils'
+	export default Vue.extend({
+		data() {
+			return {
+				showCalculator: false,
+				pickIcon: '../../static/images/home/礼物.png',
+				inputNote: '',
+				inputNum: '0',
+				cache: '',
+				editTag: '',
+				isAddOrReduce: false
+			}
+		},
+		methods: {
+			init(){
+				this.inputNote = ''
+				this.inputNum = '0'
+				this.cache = ''
+				this.editTag = ''
+				this.isAddOrReduce = false
+			},
+			close(){
+				this.showCalculator = false
+				this.init()
+			},
+			show(){
+				this.showCalculator = true
+				this.init()
+			},
+			handleInputNumber(num:string){
+				if(this.isAddOrReduce){
+					this.cache = this.inputNum
+					this.isAddOrReduce = false
+					this.inputNum = '0'
+				}
+				if(Number(this.inputNum) === 0 && this.inputNum.indexOf('.') <= -1){
+					this.inputNum = num
+				}else if(Number(this.inputNum + num) >= 10000000){
+					return
+				}else if(this.inputNum.indexOf('.')>-1 && this.inputNum.split('.')[1].length >= 2){
+					return
+				}else{
+					this.inputNum += num
+				}
+			},
+			handleInputPoint(){
+				if(this.isAddOrReduce){
+					this.cache = this.inputNum
+					this.inputNum = '0.'
+					this.isAddOrReduce = false
+				}
+				if(this.inputNum.indexOf('.')>-1){
+					return
+				}else{
+					this.inputNum += '.'
+				}
+			},
+			handleDelete(){
+				if(this.inputNum.length >= 2 && Number(this.inputNum)!==0){
+					this.inputNum = this.inputNum.substr(0,this.inputNum.length-1)
+				}else{
+					this.inputNum = '0'
+				}
+			},
+			handleAdd(){
+				if(this.cache){
+					switch(this.editTag){
+						case 'add': {
+							if((Number(this.inputNum) + Number(this.cache)) >= 10000000){
+								uni.showToast({title: '结果超出最大限制',icon:'none'})
+								this.inputNum = this.cache
+							}else{
+								this.inputNum = (Number(this.inputNum) + Number(this.cache)).toString()
+							}
+							break;
+						}
+						case 'reduce':{
+							if((Number(this.cache) - Number(this.inputNum))<0){
+								uni.showToast({title: '结果小于零',icon:'none'})
+								this.inputNum = this.cache
+							}else{
+								this.inputNum = (Number(this.cache) - Number(this.inputNum)).toString()
+							}
+							break;
+						}
+					}
+					this.cache = ''
+				}
+				this.isAddOrReduce = true
+				this.editTag = 'add'
+			},
+			handleReduce(){
+				if(this.cache){
+					switch(this.editTag){
+						case 'add': {
+							if((Number(this.inputNum) + Number(this.cache)) >= 10000000){
+								uni.showToast({title: '结果超出最大限制',icon:'none'})
+								this.inputNum = this.cache
+							}else{
+								this.inputNum = (Number(this.inputNum) + Number(this.cache)).toString()
+							}
+							break;
+						}
+						case 'reduce':{
+							if((Number(this.cache) - Number(this.inputNum))<0){
+								uni.showToast({title: '结果小于零',icon:'none'})
+								this.inputNum = this.cache
+							}else{
+								this.inputNum = (Number(this.cache) - Number(this.inputNum)).toString()
+							}
+							break;
+						}
+					}
+					this.cache = ''
+				}
+				this.isAddOrReduce = true
+				this.editTag = 'reduce'
+			},
+			handleEqual(){
+				if(this.cache){
+					switch(this.editTag){
+						case 'add': {
+							if((Number(this.inputNum) + Number(this.cache)) >= 10000000){
+								uni.showToast({title: '结果超出最大限制',icon:'none'})
+								this.inputNum = this.cache
+							}else{
+								this.inputNum = (Number(this.inputNum) + Number(this.cache)).toString()
+							}
+							break;
+						}
+						case 'reduce':{
+							if((Number(this.cache) - Number(this.inputNum))<0){
+								uni.showToast({title: '结果小于零',icon:'none'})
+								this.inputNum = this.cache
+							}else{
+								this.inputNum = (Number(this.cache) - Number(this.inputNum)).toString()
+							}
+							break;
+						}
+					}
+					this.cache = ''
+					this.editTag = ''
+				}
+			},
+			handleOk(){
+				if(Number(this.inputNum)===0){
+					uni.showToast({title: '金额不能为0',icon:'none'})
+				}else{
+					uni.showToast({title: 'submit',icon:'none'})
+					this.close()
+				}
+			}
+		}
+	})
+</script>
+
+<style lang="scss">
+	.calculator{
+		
+		.calculator-info{
+			width: 100%;
+			height: 100rpx;
+			background: #f3f3f3;
+			
+			.note{
+				padding-left: 20rpx !important;
+				flex-direction: row;
+				display: flex;
+				border-right: none;
+			}
+			
+			.money-num{
+				border-left: none;
+				font-size: 18px;
+				padding-right: 20rpx !important;
+			}
+		}
+		
+		.u-col{
+			background: #f3f3f3;
+			box-sizing: border-box;
+			border: 1rpx solid #d0d0d0;
+			height: 100rpx;
+			font-size: 14px;
+			
+			&.handle-submit{
+				background: #ffbb00;
+			}
+			
+			.choose{
+				display: flex;
+				align-items: center;
+				font-size: 12px;
+				box-sizing: border-box;
+				text{
+					padding-left: 4px;
+				}
+			}
+		}
+	}
+</style>
--- src/pages/money/Money.vue
+++ src/pages/money/Money.vue
@@ -1,5 +1,23 @@
 <template>
-	<view>
+	<view class="money-view">
+		<view class="money-body">
+			<view class="money-body-top">
+				<u-button type="primary" color="#ffbb00" text="分析" :customStyle="buttonStyle"></u-button>
+			</view>
+			<view class="money-body-icons">
+				<view class="money-body-icon" v-for="(u,index) in iconsList" :key="u.id" @click="handleShowCalculator(u)">
+					<image :src="u.src"></image>
+					<text>{{ u.name + ': ' + u.money }}</text>
+				</view>
+			</view>
+			<view class="money-body-bottom">
+				<image src=""></image>
+				<view class="today">
+					<text>今日总支出：<br> <text style="color: #ffbb00">{{ showSwitch ? todayMoney : '***' }}</text></text>
+					<u-switch style="margin-top: 20rpx;" v-model="showSwitch" size="20" activeColor="#ffbb00"></u-switch>
+				</view>
+			</view>
+		</view>
 		<u-tabbar :value="selected" :fixed="true" :placeholder="true" @change="handleChangeIndex"
 			:safeAreaInsetBottom="true" activeColor="#ffbb00" inactiveColor="#333">
 			<u-tabbar-item text="首页" icon="home-fill"></u-tabbar-item>
@@ -7,20 +25,109 @@
 			<u-tabbar-item text="备忘" icon="calendar-fill"></u-tabbar-item>
 			<u-tabbar-item text="我的" icon="account-fill"></u-tabbar-item>
 		</u-tabbar>
+		<Calculator ref="Calculator"></Calculator>
 	</view>
 </template>
 
 <script lang="ts">
 	import Vue from 'vue';
+	import Calculator from'./Calculator.vue'
 	export default Vue.extend({
+		components: {
+			Calculator
+		},
 		data() {
 			return {
+				todayMoney: 0,
+				showSwitch: false,
 				selected: 1,
 				urls: [
 					'/pages/index/Index',
 					'/pages/money/Money',
 					'/pages/memo/Memo',
 					'/pages/mine/Mine'
+				],
+				buttonStyle: {
+					height: '50rpx',
+					width: '110rpx',
+					position: 'absolute',
+					top: '22rpx',
+					right: '22rpx',
+					color: '#333'
+				},
+				iconsList: [
+					{
+						id: '1',
+						src: require('@/static/images/home/汉堡.png'),
+						name: '吃饭',
+						money: 0
+					},
+					{
+						id: '2',
+						src: require('@/static/images/home/荔枝.png'),
+						name: '果蔬',
+						money: 0
+					},
+					{
+						id: '3',
+						src: require('@/static/images/home/裙子.png'),
+						name: '衣服',
+						money: 0
+					},
+					{
+						id: '4',
+						src: require('@/static/images/home/奥迪.png'),
+						name: '汽车',
+						money: 0
+					},
+					{
+						id: '5',
+						src: require('@/static/images/home/淘宝.png'),
+						name: '淘宝',
+						money: 0
+					},
+					{
+						id: '6',
+						src: require('@/static/images/home/购物车.png'),
+						name: '超市',
+						money: 0
+					},
+					{
+						id: '7',
+						src: require('@/static/images/home/水滴.png'),
+						name: '水费',
+						money: 0
+					},
+					{
+						id: '8',
+						src: require('@/static/images/home/闪电.png'),
+						name: '电费',
+						money: 0
+					},
+					{
+						id: '9',
+						src: require('@/static/images/home/便利店.png'),
+						name: '便利店',
+						money: 0
+					},
+					{
+						id: '10',
+						src: require('@/static/images/home/地铁.png'),
+						name: '交通',
+						money: 0
+					},
+					{
+						id: '11',
+						src: require('@/static/images/home/礼物.png'),
+						name: '礼物',
+						money: 0
+					},
+					{
+						id: '12',
+						src: require('@/static/images/home/其他.png'),
+						name: '其他',
+						money: 0
+					}
 				]
 			};
 		},
@@ -28,6 +135,12 @@
 			this.selected = 1
 		},
 		methods:{
+			handleShowCalculator(record: any){
+				(this.$refs.Calculator as any).show()
+			},
+			changeSwitch(){
+				this.showSwitch = !this.showSwitch
+			},
 			handleChangeIndex(index:number){
 				console.log('12',index)
 				this.selected = index
@@ -44,5 +157,86 @@
 </script>
 
 <style lang="scss">
-
+	.money-view{
+		width: 100vw;
+		height: 100vh;
+		display: flex;
+	}
+.money-body{
+	width: 100vw;
+	flex: 1 1 1;
+	box-sizing: border-box;
+	//background: url(../../static/back-money.jpg) 60%;
+	background-size: cover;
+	position: relative;
+	
+	.money-body-top{
+		height: 90rpx;
+		width: 100vw;
+		text-align: right;
+		border-bottom: 1px solid #ffbb00;
+		position: relative;
+	}
+	
+	.money-body-icons{
+		width: 100%;
+		display: flex;
+		flex-wrap: wrap;
+		padding: 40rpx;
+		box-sizing: border-box;
+		justify-content: space-between;
+		.money-body-icon{
+			width: 150rpx;
+			height: 150rpx;
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			font-size: 12px;
+			margin-bottom: 10rpx;
+			image{
+				width: 80rpx;
+				height: 80rpx;
+				margin-bottom: 8rpx;
+			}
+			text{
+				text-align: center;
+			}
+		}
+	}
+	
+	.money-body-bottom{
+		position: absolute;
+		width: 100vw;
+		bottom: 0;
+		left: 0;
+		height: 500rpx;
+		display: flex;
+		justify-content: space-between;
+		
+		image{
+			width: 360rpx;
+			height: 360rpx;
+			border-radius: 50%;
+			margin-top: 10rpx;
+			margin-left: 30rpx;
+		}
+		
+		.today{
+			margin-top: 100rpx;
+			margin-right: 40rpx;
+			padding: 20rpx;
+			box-sizing: border-box;
+			background: rgba(255,255,255,0.05);
+			color: #fff;
+			font-size: 16px;
+			border-radius: 20rpx;
+			width: 260rpx;
+			height: 200rpx;
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+		}
+	}
+}
 </style>
